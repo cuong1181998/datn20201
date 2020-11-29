3bdbac120432b24a8d5a4a4051c2cb94be44a137
cuonghxicts <cuong1181998@gmail.com>
2020-11-29 17:19:58 +0700

    clean-test

--- src/handle_img.py
+++ src/handle_img.py
@@ -1,31 +1,50 @@
 import time
 import cv2
+import os
+import numpy as np
+ 
 try :
-    from text_detect import load_detector, extract_text_box, resize_img
-    from text_recognize import load_recognizer, extract_text
-    from detection.imgproc import loadImage
-    from opt import get_config
+   from text_detect import load_detector, extract_text_box, resize_img
+   from text_recognize import load_recognizer, extract_text
+   from detection.imgproc import loadImage
+   from opt import get_config
 except ImportError:
-    from src.text_detect import load_detector, extract_text_box, resize_img
-    from src.text_recognize import load_recognizer, extract_text
-    from src.detection.imgproc import loadImage
-    from src.opt import get_config
-
-def main(args,image_path):
-    net, refine_net = load_detector()
-    model, converter = load_recognizer()
-
-    image = loadImage(image_path)
-    # image = resize_img(image,640)
-    image = cv2.resize(image,(480,960))
-    # t_start = time.time()
-    bboxes, polys, score_text = extract_text_box(net, image, args.text_threshold,
+   from src.text_detect import load_detector, extract_text_box, resize_img
+   from src.text_recognize import load_recognizer, extract_text
+   from src.detection.imgproc import loadImage
+   from src.opt import get_config
+ 
+def get_images(folder):
+   files = []
+   exts = ['jpg', 'png', 'jpeg', 'JPG']
+   for parent, dirnames, filenames in os.walk(folder):
+       for filename in filenames:
+           for ext in exts:
+               if filename.endswith(ext):
+                   files.append(os.path.join(parent, filename))
+                   break
+   print('Find {} images'.format(len(files)))
+   return files
+  
+def main(args,folder):
+   net, refine_net = load_detector()
+   model, converter = load_recognizer()
+ 
+   im_fn_list = get_images(folder)
+   for im_fn in im_fn_list:
+     im = loadImage(im_fn)
+     image = cv2.resize(im,(480,960))
+     bboxes, polys, score_text = extract_text_box(net, image, args.text_threshold,
                                                  args.link_threshold, args.low_text,
                                                  args.cuda, args.poly, refine_net)
-    text = extract_text(model, converter, image, polys, image_path)
-    print(text)
-
+     text = extract_text(model, converter, image, polys, im_fn)
+     for raw in text:
+       box = np.array([int(raw[1]), int(raw[2]), int(raw[3]), int(raw[4]), int(raw[5]), int(raw[6]), int(raw[7]), int(raw[8])])
+       title = raw[9];
+       cv2.polylines(image, [box.reshape((-1, 1, 2))], True, color=(0, 0, 255), thickness=2)  
+       cv2.putText(image, title, (box[0],box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 1, cv2.LINE_AA)
+     print(os.path.join('../result', os.path.basename(im_fn)))
+     cv2.imwrite(os.path.join('../result', os.path.basename(im_fn)),image)
 if __name__ == '__main__':
-    args = get_config()
-    img_path = "/home/son/Desktop/datn20201/resource/img/receipt_1007.jpg"
-    main(args,img_path)
+   args = get_config()
+   main(args,args.img_path)
--- src/text_recognize.py
+++ src/text_recognize.py
@@ -1,124 +1,139 @@
+ 
 import string
-
+ 
 import torch
 import torch.backends.cudnn as cudnn
 import torch.utils.data
 import torch.nn.functional as F
-
+ 
 import pandas as pd
 import numpy as np
 import csv
-
+ 
 try:
-    from recognition.utils import CTCLabelConverter, AttnLabelConverter
-    from recognition.dataset import RawDataset, AlignCollate, Image2TextDataset
-    from recognition.model import Model
-    from opt import get_config
-    from detection.imgproc import sorting_bounding_box
+   from recognition.utils import CTCLabelConverter, AttnLabelConverter
+   from recognition.dataset import RawDataset, AlignCollate, Image2TextDataset
+   from recognition.model import Model
+   from opt import get_config
+   from detection.imgproc import sorting_bounding_box
 except ImportError:
-    from src.recognition.utils import CTCLabelConverter, AttnLabelConverter
-    from src.recognition.dataset import RawDataset, AlignCollate, Image2TextDataset
-    from src.recognition.model import Model
-    from src.opt import get_config
-    from src.detection.imgproc import sorting_bounding_box
-
+   from src.recognition.utils import CTCLabelConverter, AttnLabelConverter
+   from src.recognition.dataset import RawDataset, AlignCollate, Image2TextDataset
+   from src.recognition.model import Model
+   from src.opt import get_config
+   from src.detection.imgproc import sorting_bounding_box
+ 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+ 
 ###load_config
 args = get_config()
-
+ 
 def load_recognizer():
-    """ model configuration """
-    if args.sensitive:
-        args.character = string.printable[:-6]
-
-    if 'CTC' in args.Prediction:
-        converter = CTCLabelConverter(args.character)
-    else:
-        converter = AttnLabelConverter(args.character)
-    args.num_class = len(converter.character)
-
-    if args.rgb:
-        args.input_channel = 3
-    model = Model(args)
-    print('model input parameters', args.imgH, args.imgW, args.num_fiducial, args.input_channel, args.output_channel,
-          args.hidden_size, args.num_class, args.batch_max_length, args.Transformation, args.FeatureExtraction,
-          args.SequenceModeling, args.Prediction)
-    model = torch.nn.DataParallel(model).to(device)
-
-    # load model
-    # print('loading pretrained model from %s' % args.saved_model)
-    model.load_state_dict(torch.load(args.saved_model, map_location=device))
-    return model, converter
-
+   """ model configuration """
+   if args.sensitive:
+       args.character = string.printable[:-6]
+ 
+   if 'CTC' in args.Prediction:
+       converter = CTCLabelConverter(args.character)
+   else:
+       converter = AttnLabelConverter(args.character)
+   args.num_class = len(converter.character)
+ 
+   if args.rgb:
+       args.input_channel = 3
+   model = Model(args)
+   print('model input parameters', args.imgH, args.imgW, args.num_fiducial, args.input_channel, args.output_channel,
+         args.hidden_size, args.num_class, args.batch_max_length, args.Transformation, args.FeatureExtraction,
+         args.SequenceModeling, args.Prediction)
+   model = torch.nn.DataParallel(model).to(device)
+ 
+   # load model
+   print('loading pretrained model from %s' % args.saved_model)
+   model.load_state_dict(torch.load(args.saved_model, map_location=device))
+   return model, converter
+ 
 def extract_text(model,converter,image,poly,image_path):
+ 
+   boxes_sorted_list, num_rows_dict = sorting_bounding_box(poly)
+   text_results = []
+   # prepare data
+   AlignCollate_demo = AlignCollate(imgH=args.imgH, imgW=args.imgW, keep_ratio_with_pad=args.PAD)
+   # demo_data = RawDataset(root=args.image_folder, opt=args)  # use RawDataset
+   demo_data = Image2TextDataset(image,boxes_sorted_list,args)
+ 
+   demo_loader = torch.utils.data.DataLoader(
+       demo_data, batch_size=args.batch_size,
+       shuffle=False,
+       num_workers=int(args.workers),
+       collate_fn=AlignCollate_demo, pin_memory=True)
+ 
+   # predict
+   model.eval()
+   with torch.no_grad():
+       for image_tensors, labels in demo_loader:
+           batch_size = image_tensors.size(0)
+           image = image_tensors.to(device)
+           # For max length prediction
+           length_for_pred = torch.IntTensor([args.batch_max_length] * batch_size).to(device)
+           text_for_pred = torch.LongTensor(batch_size, args.batch_max_length + 1).fill_(0).to(device)
+           if 'CTC' in args.Prediction:
+               preds = model(image, text_for_pred)
+ 
+               # Select max probabilty (greedy decoding) then decode index to character
+               preds_size = torch.IntTensor([preds.size(1)] * batch_size)
+               _, preds_index = preds.max(2)
+               # preds_index = preds_index.view(-1)
+               preds_str = converter.decode(preds_index, preds_size)
+ 
+           else:
+               preds = model(image, text_for_pred, is_train=False)
+ 
+               # select max probabilty (greedy decoding) then decode index to character
+               _, preds_index = preds.max(2)
+               preds_str = converter.decode(preds_index, length_for_pred)
+ 
+           log = open(f'./log_demo_result.txt', 'a')
+           dashed_line = '-' * 80
+           head = f'{"image_path":25s}\t{"predicted_labels":25s}\tconfidence score'           
+           print(f'{dashed_line}\n{head}\n{dashed_line}')
+           log.write(f'{dashed_line}\n{head}\n{dashed_line}\n')
+           preds_prob = F.softmax(preds, dim=2)
+           preds_max_prob, _ = preds_prob.max(dim=2)
+           for pred, pred_max_prob in zip(preds_str, preds_max_prob):
+               if 'Attn' in args.Prediction:
+                   pred_EOS = pred.find('[s]')
+                   pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
+                   pred_max_prob = pred_max_prob[:pred_EOS]
+               text_results.append(pred)
+               # calculate confidence score (= multiply of pred_max_prob)
+               confidence_score = pred_max_prob.cumprod(dim=0)[-1]
+               log.write(f'{pred:25s}\t{confidence_score:0.4f}\n')
+           log.close()
+ 
+       res = ""
+       tmp = 0
+       img2csv = []
+       text = []
+       for key, value in num_rows_dict.items():
+           for i in value:
+           #     res = res + (" ".join(text_results[tmp:tmp+i[0]])) + ' '
+           #
+           # res += '\n'
+               flatten_arr = np.array(i[1]).flatten()
+               point_2_string = list(map(str, flatten_arr))
+               img2csv.append([1,*point_2_string," ".join(text_results[tmp:tmp+i[0]])])
+               text.append([" ".join(text_results[tmp:tmp+i[0]])])
+               tmp += i[0]
+   return img2csv
+ 
+# if __name__ == '__main__':
+#       # same with ASTER setting (use 94 char).
+#
+#     cudnn.benchmark = True
+#     cudnn.deterministic = True
+#     args.num_gpu = torch.cuda.device_count()
+#
+#     model, converter = load_recognizer()
+#     extract_text(model, converter)
+ 
 
-    boxes_sorted_list, num_rows_dict = sorting_bounding_box(poly)
-    text_results = []
-    # prepare data
-    AlignCollate_demo = AlignCollate(imgH=args.imgH, imgW=args.imgW, keep_ratio_with_pad=args.PAD)
-    # demo_data = RawDataset(root=args.image_folder, opt=args)  # use RawDataset
-    demo_data = Image2TextDataset(image,boxes_sorted_list,args)
-
-    demo_loader = torch.utils.data.DataLoader(
-        demo_data, batch_size=args.batch_size,
-        shuffle=False,
-        num_workers=int(args.workers),
-        collate_fn=AlignCollate_demo, pin_memory=True)
-
-    # predict
-    model.eval()
-    with torch.no_grad():
-        for image_tensors, labels in demo_loader:
-            batch_size = image_tensors.size(0)
-            image = image_tensors.to(device)
-            # For max length prediction
-            length_for_pred = torch.IntTensor([args.batch_max_length] * batch_size).to(device)
-            text_for_pred = torch.LongTensor(batch_size, args.batch_max_length + 1).fill_(0).to(device)
-            if 'CTC' in args.Prediction:
-                preds = model(image, text_for_pred)
-
-                # Select max probabilty (greedy decoding) then decode index to character
-                preds_size = torch.IntTensor([preds.size(1)] * batch_size)
-                _, preds_index = preds.max(2)
-                # preds_index = preds_index.view(-1)
-                preds_str = converter.decode(preds_index, preds_size)
-
-            else:
-                preds = model(image, text_for_pred, is_train=False)
-
-                # select max probabilty (greedy decoding) then decode index to character
-                _, preds_index = preds.max(2)
-                preds_str = converter.decode(preds_index, length_for_pred)
-
-            log = open(f'./log_demo_result.txt', 'a')
-            dashed_line = '-' * 80
-            head = f'{"image_path":25s}\t{"predicted_labels":25s}\tconfidence score'            
-            # print(f'{dashed_line}\n{head}\n{dashed_line}')
-            log.write(f'{dashed_line}\n{head}\n{dashed_line}\n')
-            preds_prob = F.softmax(preds, dim=2)
-            preds_max_prob, _ = preds_prob.max(dim=2)
-            for pred, pred_max_prob in zip(preds_str, preds_max_prob):
-                if 'Attn' in args.Prediction:
-                    pred_EOS = pred.find('[s]')
-                    pred = pred[:pred_EOS]  # prune after "end of sentence" token ([s])
-                    pred_max_prob = pred_max_prob[:pred_EOS]
-                text_results.append(pred)
-                # calculate confidence score (= multiply of pred_max_prob)
-                confidence_score = pred_max_prob.cumprod(dim=0)[-1]
-                # print(f'{pred:25s}\t{confidence_score:0.4f}')
-                log.write(f'{pred:25s}\t{confidence_score:0.4f}\n')
-            log.close()
-
-        res = ""
-        tmp = 0
-        img2csv = []
-        for key, value in num_rows_dict.items():
-            for i in value:
-                # res = res + ("\n".join(text_results[tmp:tmp+i[0]])).upper() + '\n' // convert uppercase if SROIE data set
-                res = res + ("\n".join(text_results[tmp:tmp+i[0]]))+ '\n'
-                tmp += i[0]
-                
-        with open('./result'+image_path.split('/')[-1][:-3]+'txt', "w") as outfile:
-          outfile.write(res)
-    return res
